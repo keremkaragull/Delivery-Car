dc6c4d5445cb41a696b9f7eb67f44f415c90258e
umutkurtull <umutkurtull@gmail.com>
2024-01-11 20:30:39 +0300

    bug fix
    ananinami

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -11,16 +11,26 @@ public class Driver : MonoBehaviour
     [SerializeField] float moveSpeed = 10f;
     [SerializeField] float slowSpeed = 5f;
     [SerializeField] float boostSpeed = 20f;
+
     void Update()
     {
-    if(!Mathf.Approximately(Input.GetAxis("Vertical"), 0 ))
-    {
-        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
-        transform.Rotate(0, 0, -steerAmount);
+        if(!Mathf.Approximately(Input.GetAxis("Vertical"), 0 ))
+        {
+            float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+            if (Input.GetAxis("Vertical") == 1)
+            {
+                transform.Rotate(0, 0, -steerAmount);
+            }
+            else if (Input.GetAxis("Vertical") == -1)
+            {
+                transform.Rotate(0, 0, steerAmount);
+            }
+
+        }
+            float moveAmount = Input.GetAxis("Vertical") * moveSpeed  * Time.deltaTime;
+            transform.Translate(0, moveAmount, 0);
     }
-        float moveAmount = Input.GetAxis("Vertical") * moveSpeed  * Time.deltaTime;
-        transform.Translate(0, moveAmount, 0);
-    }
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "SpeedUp" && moveSpeed != boostSpeed)
